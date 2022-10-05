ef919183ea3edebdd0cc1b103fbf516bb988d77c
Alumno Administración <aadmon@utags.edu.mx>
2022-10-05 18:02:55 -0500

    cambios 05-10-2022

--- js/script.js
+++ js/script.js
@@ -36,7 +36,7 @@ const verbsCounter = document.getElementById('verbs-counter');
 const allRigthCounter = document.getElementById('all-right-counter');
 const verbsContainer = document.getElementById('verbs-container');
 
-const positionOfVerbs = verbs.lenght;
+const positionOfVerbs = verbs.length;
 
 let answerRoullete = [0,1,1,1];
 let numberOfVerbs = [];
@@ -49,7 +49,37 @@ next.addEventListener('click', () => {
 });
 
 function ponerVerbo(){
-    //verbsContainer.innerHTML = "<h1>Deposita 200 bitcoins para jugar</h1>";
+    answerRoullete = suffleAnswers(answerRoullete);
+    let randomPosition = numberOfVerbs[lastPosition];
+    let imgText =  `<img src='img/${verbs[randomPosition]}.jpg' height: '140px' width:'100px'>`;
+
+    first.classList.add("btn", "btn-outline-primary", "btn-md");
+    second.classList.add("btn", "btn-outline-primary", "btn-md");
+    third.classList.add("btn", "btn-outline-primary", "btn-md");
+    fourth.classList.add("btn", "btn-outline-primary", "btn-md");
+
+    if (lastPosition >= 0) {
+        var just_position = lastPosition + 1;
+        verbsCounter.innerHTML = '' + just_position + ' / ' + numberOfVerbs;
+        allRigthCounter.innerHTML = "Right Answers: " + rightAnswerCounter;
+        showVerb.innerHTML = verbs[randomPosition];
+        showImage.innerHTML = imgText;
+        showAudio.src = `audio/${verbs[randomPosition]}.mp3`;
+        showAudio.play();
+
+        first.innerHTML = !answerRoullete[0]?verbos[randomPosition]:verbos[randomVerb];
+        second.innerHTML = !answerRoullete[0]?verbos[randomPosition]:verbos[randomVerb];
+        third.innerHTML = !answerRoullete[0]?verbos[randomPosition]:verbos[randomVerb];
+        fourth.innerHTML = !answerRoullete[0]?verbos[randomPosition]:verbos[randomVerb];
+
+        rightAnswer = verbos[randomPosition];
+        lastPosition = lastPosition-1;
+    } else {
+        verbsCounter.innerHTML = "0 / " + numberOfVerbs;
+        allRigthCounter.innerHTML = "Right Answers: " + rightAnswerCounter;
+        showVerb.innerHTML = "Thank you!";
+        verbsContainer.innerHTML = "";
+    }
 }
 
 makeRandomList();
@@ -107,3 +137,14 @@ function suffleAnswers(array){
         [array[numberOfAnswersButtons],array[randomIndex]] = [array[randomIndex], array[numberOfAnswersButtons]];
     }
 }
+
+function isItRight_(){
+    return answer == rightAnswer?true:false;
+}
+
+function randomVerb(notThisOne){
+    theOne = Math.floor(Math.random()*verbsContainer.length);
+    return theOne == notThisOne?randomVerb(notThisOne):theOne;
+}
+
+
---
+++ js/verbs.js
@@ -0,0 +1,1 @@
+let verbs = ["open", "close"];
